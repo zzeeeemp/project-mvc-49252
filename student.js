90a7ef85141665e517a46e902d66c2a2c2080f73
zzeeeemp <vladguk5443@gmail.com>
2024-03-12 14:24:01 +0100

    zadanie2

---
+++ student.js
@@ -0,0 +1,12 @@
+const FULLNAME = "Vladyslav Huk";
+const STUDENT_ID = "49252";
+
+function getStudentFullName() {
+  return FULLNAME;
+}
+
+function getStudentId() {
+  return STUDENT_ID;
+}
+
+console.log(`My name is ${getStudentFullName()}. My student ID is ${getStudentId()}`);
